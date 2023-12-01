0b4e1d837bddb950fc857e55e2142a4e61068fed
newtondotcom <asphalt9fr@gmail.com>
2023-12-01 09:15:24 +0100

    add hdfs server

---
+++ src/hdfs/HdfsServer.java
@@ -0,0 +1,12 @@
+package hdfs;
+
+public class HdfsServer {
+
+	public static void HdfsRead(String fname) {
+	}
+
+	public static void main(String[] args) {
+		// java HdfsClient <read|write> <txt|kv> <file>
+		// appel des méthodes précédentes depuis la ligne de commande
+	}
+}
