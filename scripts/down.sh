9db417191fc1e156456cedb6a67bceac2a8175ca
newtondotcom <asphalt9fr@gmail.com>
2023-12-20 09:36:55 +0100

    echo steps + new config path

--- scripts/down.sh
+++ scripts/down.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # récupérer la 2e ligne du fichier de config (noms des machines)
-listepc=$(sed "2q;d" src/config/config_hidoop.cfg)
+listepc=$(sed "2q;d" src/config/main.cfg)
 
 # découper la chaîne obtenue sur les délimiteurs ","
 # et la stocker dans un tableau
@@ -10,6 +10,8 @@ IFS=',' read -ra tabpc <<< "$listepc"
 # Arrêter les démons hidoop et hdfs sur les machines distantes
 for pc in "${tabpc[@]}"
 do
+	echo "Arrêt du démon Hagidoop sur $pc"
 	ssh raugerea2@$pc "kill \$(jps | grep DaemonImpl | awk '{print \$1}')"
+	echo "Arrêt du démon HdfsServer sur $pc"
 	ssh raugerea2@$pc "kill \$(jps | grep HdfsServer | awk '{print \$1}')"
 done
