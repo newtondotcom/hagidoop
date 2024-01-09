859a139ce0af8dbb3dccad3ee10d805b9a228af7
newtondotcom <asphalt9fr@gmail.com>
2024-01-09 21:26:58 +0100

    read and parse config file in java

--- src/config/Project.java
+++ src/config/Project.java
@@ -1,6 +1,24 @@
 package config;
 
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Project {
-	public static String PATH = "/home/hagimont/hagidoop/";
-	public static String TEMP_PATH = "/tmp/";
+    public static String PATH = "/home/raugerea2/Téléchargements/Hagidoop/";
+    public static String TEMP_PATH = "/tmp/";
+
+    private static String nameNode = "main.cfg";
+
+    public static void main(String[] args) {
+        Map<String, Integer> hash = new HashMap<>();
+        try {
+            FileOutputStream fichier = new FileOutputStream(nameNode);
+            ObjectOutputStream objet = new ObjectOutputStream(fichier);
+            objet.writeObject(hash);
+            objet.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
