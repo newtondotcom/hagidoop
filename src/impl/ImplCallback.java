29a19e2a91d12f7314f5398501b1961c9eeed3b6
newtondotcom <asphalt9fr@gmail.com>
2024-01-10 22:27:09 +0100

    indent

--- src/impl/ImplCallback.java
+++ src/impl/ImplCallback.java
@@ -5,10 +5,10 @@ import interfaces.Callback;
 
 public class ImplCallback implements Callback {
 
-		private String adresseMain; // Adresse pour contacter le main et prevenir que l'on a fini
+	private String adresseMain; // Adresse pour contacter le main et prevenir que l'on a fini
     private int id; // Id du Callback, le worker saura sur quel fragment il travaille
 
-  /* Constructor */
+  	/* Constructor */
 	public ImplCallback(int _id, String _adresseMain) {
 		this.id = _id;
 		this.adresseMain = _adresseMain;
