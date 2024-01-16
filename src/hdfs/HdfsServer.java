e63604f8d3d358565efa28b75c9991f8c0453b1a
newtondotcom <asphalt9fr@gmail.com>
2024-01-17 00:15:59 +0100

    rm pleins de trucs

--- src/hdfs/HdfsServer.java
+++ src/hdfs/HdfsServer.java
@@ -1,5 +1,6 @@
 package hdfs;
 
+import config.Project;
 import impl.ImplFileRW;
 import interfaces.FileReaderWriter;
 import interfaces.KV;
@@ -13,17 +14,20 @@ public class HdfsServer {
 
 	public static void main(String[] args) {
         try {
-            // Récupération du port
+            // Port avec ligne de commande
             int port = Integer.parseInt(args[0]);
 
             // Ouverture du socket côté serveur
             ServerSocket waitingsocket = new ServerSocket(port);
 
-            // Boucle infinie pour accepter les connexions des clients
+            // Boucle infinie pour accepter les connexions des clients V
             Boolean running = true; // variable pour lancer le script sinon erreur Java
             while (running) {
+                // Attente d'une connexion d'un client
                 Socket socket = waitingsocket.accept();
                 ObjectInputStream objectIS = new ObjectInputStream(socket.getInputStream());
+
+                // Récupération du message envoyé par le client
                 String msg = (String) objectIS.readObject();
                 String[] req = msg.split("#");
                 String commande = req[0];
@@ -32,20 +36,24 @@ public class HdfsServer {
                 if (extension.equals("kv")) {
                 type = FileReaderWriter.FMT_KV;
                 }
-                if(commande.equals(":WRITE")){
-                File folder = new File("/tmp/data/");
-                Boolean exists = folder.mkdir();
-                ImplFileRW fileRW = new ImplFileRW(0, "/tmp/data/"+req[1], type);
-                fileRW.open("w");
-                String content;
-                if (req.length > 2){
-                    content = req[2];
-                } else {
-                    content = "";
-                }
-                fileRW.write(content.trim());
-                fileRW.close();
-                } else if (commande.equals(":READ")){
+
+                // Commandes
+                if(commande.equals(Project.CommandPrefix +"WRITE")){
+                    // Ecriture du fichier
+                    File folder = new File("/tmp/data/");
+                    Boolean exists = folder.mkdir();
+                    ImplFileRW fileRW = new ImplFileRW(0, "/tmp/data/"+req[1], type);
+                    fileRW.open("w");
+                    String content;
+                    if (req.length > 2){
+                        content = req[2];
+                    } else {
+                        content = "";
+                    }
+                    fileRW.write(content.trim());
+                    fileRW.close();
+                } else if (commande.equals(Project.CommandPrefix+"READ")){
+                    // Lecture du fichier
                     ImplFileRW fileRW2 = new ImplFileRW(0, "/tmp/data/"+req[1], type);
                         fileRW2.open("r");
                         StringBuilder fragment = new StringBuilder();
@@ -54,7 +62,6 @@ public class HdfsServer {
                             d = String.valueOf(fileRW2.readtxt());
                             if (!Objects.equals(d, "null") && d != null) {
                                 fragment.append(d).append("\n");
-                                System.out.println(d);
                             } else {
                                 break;
                             }
@@ -63,11 +70,12 @@ public class HdfsServer {
                         objectos.writeObject(fragment.toString());
                         fileRW2.close();
                         objectos.close();
-                } else if (commande.equals(":DELETE")){
+                } else if (commande.equals(Project.CommandPrefix+"DELETE")){
+                    // Suppression du fichier
                     File file = new File("/tmp/data/"+req[1]);
                     boolean deleted = file.delete();
                 } else {
-                    System.out.println("Commande inconnue");
+                    System.out.println("ATTENTION Commande inconnue");
                 }
                 objectIS.close();
                 socket.close();
